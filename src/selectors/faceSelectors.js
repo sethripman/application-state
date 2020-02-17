22d8890302216fd04c5dca4a6828e7b208a31707
sethripman <sethripman@gmail.com>
2020-02-17 13:34:02 -0800

    working on tests passing

---
+++ src/selectors/faceSelectors.js
@@ -0,0 +1,17 @@
+export const isTired = state => state.coffees < 1 && state.naps < 1;
+export const isHyper = state => state.coffees > 3;
+export const isEducated = state => state.studies > 2;
+export const isHungry = state => state.snacks < 1;
+export const isSwoll = state => state.exercises > 9;
+
+export const getFace = state => {
+  if(isSwoll(state)) return '💪';
+  if(isTired(state) && isHungry(state)) return '😠';
+  if(isHyper(state) && isHungry(state)) return '😱';
+  if(isTired(state)) return '😴';
+  if(isHyper(state)) return '🙀';
+  if(isEducated(state)) return '😲';
+  if(isHungry(state)) return '😡';
+
+  return '😀';
+};
